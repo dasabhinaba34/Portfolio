49f025efc2411ce502e8eafaf7de0c6864a06885
dasabhinaba34 <dasabhinaba34@gmail.com>
2023-02-08 20:39:52 +0530

    portfolio

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import Navbar from "./Components/Navbar";
+import Home from "./Sections/Home";
+import About from "./Sections/About";
+import Work from "./Sections/Work";
+import Contact from "./Sections/Contact";
+import ContactBar from "./Components/ContactBar";
+import Footer from "./Components/Footer";
+import "./App.css";
+
+function App() {
+	return (
+		<main className="main">
+			<Navbar />
+			<div className="app_section_container">
+				<Home />
+				<About />
+				<Work />
+				<Contact />
+			</div>
+			<ContactBar />
+			<Footer />
+		</main>
+	);
+}
+
+export default App;
---
+++ src/Components/Navbar/index.jsx
@@ -0,0 +1,28 @@
+import styles from "./styles.module.css";
+
+function Navbar() {
+	return (
+		<nav className={styles.nav_container}>
+			<div className={styles.nav_left}>
+				<a href="#home">Abhinaba Das</a>
+			</div>
+			<div className={styles.nav_right}>
+				<a href="#about" className={styles.nav_link}>
+					About
+				</a>
+				{/* <a href="#experience" className={styles.nav_link}>
+					<span>02.</span>Experience
+				</a> */}
+				<a href="#work" className={styles.nav_link}>
+					Projects
+				</a>
+				<a href="#contact" className={styles.nav_link}>
+					Contact
+				</a>
+				{/* <button className={styles.nav_resume_btn}>Resume</button> */}
+			</div>
+		</nav>
+	);
+}
+
+export default Navbar;
